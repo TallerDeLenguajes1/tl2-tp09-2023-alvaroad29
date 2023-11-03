5f1bb4adf5f3fe5b171283a4e7759ae3dfa6555e
Alvaro Diaz <alvaroad29012001@gmail.com>
2023-11-03 18:36:34 -0300

    primer commit

---
+++ Models/Usuario.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace tl2_tp09_2023_alvaroad29.Models
+{
+    public class Usuario
+    {
+        
+    }
+}
